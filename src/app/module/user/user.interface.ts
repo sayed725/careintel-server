27c4f73f8d201561e01007f6896532080d98e5f7
sayed725 <ssayed72533@gmail.com>
2026-02-15 20:22:32 +0600

    added zod validation for doctor

---
+++ src/app/module/user/user.interface.ts
@@ -0,0 +1,22 @@
+import { Gender } from "../../../generated/enums";
+
+
+
+export interface ICreateDoctorPayload {
+    password: string;
+    doctor: {
+        name: string;
+        email: string;
+        profilePhoto?: string;
+        contactNumber?: string;
+        address?: string;
+        registrationNumber: string;
+        experience?: number;
+        gender: Gender;
+        appointmentFee: number;
+        qualification: string;
+        currentWorkingPlace: string;
+        designation: string;
+    }
+    specialties: string[];
+}
