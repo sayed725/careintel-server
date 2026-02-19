734a0448c461c8582ca736f99be6d77351dfdc5d
sayed725 <ssayed72533@gmail.com>
2026-02-19 17:13:35 +0600

    added doctor shedul fun

---
+++ src/app/module/doctorSchedule/doctorSchedule.route.ts
@@ -0,0 +1,21 @@
+import { Router } from "express";
+
+import { checkAuth } from "../../middleware/checkAuth";
+import { DoctorScheduleController } from "./doctorSchedule.controller";
+import { Role } from "../../../generated/enums";
+
+
+const router = Router();
+
+router.post("/create-my-doctor-schedule",
+    checkAuth(Role.DOCTOR),
+     DoctorScheduleController.createMyDoctorSchedule);
+router.get("/my-doctor-schedules", checkAuth(Role.DOCTOR), DoctorScheduleController.getMyDoctorSchedules);
+router.get("/", checkAuth(Role.ADMIN, Role.SUPER_ADMIN), DoctorScheduleController.getAllDoctorSchedules);
+router.get("/:doctorId/schedule/:scheduleId", checkAuth(Role.ADMIN, Role.SUPER_ADMIN), DoctorScheduleController.getDoctorScheduleById);
+router.patch("/update-my-doctor-schedule",
+    checkAuth(Role.DOCTOR),
+    DoctorScheduleController.updateMyDoctorSchedule);
+router.delete("/delete-my-doctor-schedule/:id", checkAuth(Role.DOCTOR), DoctorScheduleController.deleteMyDoctorSchedule);
+
+export const DoctorScheduleRoutes = router;
