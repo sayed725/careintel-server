4415de004e46622352b9ef0e16fcd3301ab14768
sayed725 <ssayed72533@gmail.com>
2026-02-19 12:51:03 +0600

    added shecdul routes

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -4,6 +4,7 @@ import { SpecialtyRoutes } from "../module/specialty/specialty.route";
 import { UserRoutes } from "../module/user/user.route";
 import { DoctorRoutes } from "../module/doctor/doctor.route";
 import { AdminRoutes } from "../module/admin/admin.route";
+import { scheduleRoutes } from "../module/schedule/schedule.route";
 
 const router = Router();
 
@@ -12,6 +13,10 @@ router.use("/specialties", SpecialtyRoutes)
 router.use("/users", UserRoutes)
 router.use("/doctors", DoctorRoutes)
 router.use("/admins", AdminRoutes)
+
+router.use("/schedules", scheduleRoutes)
+// router.use("/doctor-schedules", DoctorScheduleRoutes)
+// router.use("/appointments", AppointmentRoutes)
 
 
 export const IndexRoutes = router;
