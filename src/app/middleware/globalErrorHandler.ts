156d255876b05b85cc75bd0836e077afd110b409
sayed725 <ssayed72533@gmail.com>
2026-02-21 23:32:41 +0600

    added profile update fun report upload fun and clodinary error delete file option

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -8,6 +8,7 @@ import { handleZodError } from "../errorHelpers/handleZodError";
 import AppError from "../errorHelpers/AppError";
 import { envVars } from "../config/env";
 import { deleteFileFromCloudinary } from "../config/cloudinary.config";
+import { deleteUploadedFilesFromGlobalErrorHandler } from "../utils/deleteUploadedFilesFromGlobalErrorHandler";
 
 
 
@@ -19,15 +20,18 @@ export const globalErrorHandler = async(err: any, req: Request, res: Response, n
     }
 
 
-    if(req.file){
-     await deleteFileFromCloudinary(req.file.path)
-    }
+    // if(req.file){
+    //  await deleteFileFromCloudinary(req.file.path)
+    // }
 
 
-    if(req.files && Array.isArray(req.files) && req.files.length > 0){
-        const imageUrls = req.files.map((file) => file.path);
-        await Promise.all(imageUrls.map(url => deleteFileFromCloudinary(url))); 
-    }
+    // if(req.files && Array.isArray(req.files) && req.files.length > 0){
+    //     const imageUrls = req.files.map((file) => file.path);
+    //     await Promise.all(imageUrls.map(url => deleteFileFromCloudinary(url))); 
+    // }
+
+    await deleteUploadedFilesFromGlobalErrorHandler(req);
+
 
 
     let errorSources: TErrorSources[] = []
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -7,18 +7,25 @@ import { AdminRoutes } from "../module/admin/admin.route";
 import { scheduleRoutes } from "../module/schedule/schedule.route";
 import { DoctorScheduleRoutes } from "../module/doctorSchedule/doctorSchedule.route";
 import { AppointmentRoutes } from "../module/appointment/appointment.route";
+import { PatientRoutes } from "../module/patient/patient.route";
 
 const router = Router();
 
 router.use("/auth", AuthRoutes);
 router.use("/specialties", SpecialtyRoutes)
 router.use("/users", UserRoutes)
+router.use("/patients", PatientRoutes)
+
 router.use("/doctors", DoctorRoutes)
 router.use("/admins", AdminRoutes)
 
 router.use("/schedules", scheduleRoutes)
 router.use("/doctor-schedules", DoctorScheduleRoutes)
 router.use("/appointments", AppointmentRoutes)
+
+
+// router.use("/prescriptions", PrescriptionRoutes)
+// router.use("/reviews", ReviewRoutes)
 
 
 export const IndexRoutes = router;
