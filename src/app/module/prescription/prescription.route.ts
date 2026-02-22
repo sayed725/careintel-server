73291515936959e3d603ffbe1586da210bdcfd97
sayed725 <ssayed72533@gmail.com>
2026-02-22 13:20:51 +0600

    added prescription function

---
+++ src/app/module/prescription/prescription.route.ts
@@ -0,0 +1,44 @@
+import express from 'express';
+
+import { checkAuth } from '../../middleware/checkAuth';
+import { validateRequest } from '../../middleware/validateRequest';
+import { PrescriptionController } from './prescription.controller';
+import { PrescriptionValidation } from './prescription.validation';
+import { Role } from '../../../generated/enums';
+
+const router = express.Router();
+
+router.get(
+    '/',
+    checkAuth(Role.SUPER_ADMIN, Role.ADMIN),
+    PrescriptionController.getAllPrescriptions
+);
+
+router.get(
+    '/my-prescriptions',
+    checkAuth(Role.PATIENT, Role.DOCTOR),
+    PrescriptionController.myPrescriptions
+)
+
+router.post(
+    '/',
+    checkAuth(Role.DOCTOR),
+    validateRequest(PrescriptionValidation.createPrescriptionZodSchema),
+    PrescriptionController.givePrescription
+)
+
+router.patch(
+    '/:id',
+    checkAuth(Role.DOCTOR),
+    validateRequest(PrescriptionValidation.updatePrescriptionZodSchema),
+    PrescriptionController.updatePrescription
+)
+
+router.delete(
+    '/:id',
+    checkAuth(Role.DOCTOR),
+    PrescriptionController.deletePrescription
+)
+
+
+export const PrescriptionRoutes = router;
